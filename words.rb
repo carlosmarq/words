2878942fb09a6d6436666ba37f388ec0b791e8fd
Carlos <carlosmarq@gmail.com>
2017-10-11 20:34:43 -0500

    Reading from file

---
+++ words.rb
@@ -0,0 +1,41 @@
+@line_num=0
+@ALLwords = []
+@ALLcombinations = []
+File.open('words.txt').each do |line|
+  @line_num=@line_num+1
+  @y0=line
+  @y0.delete!("\n")
+#  print @y0
+  @ALLwords << @y0
+end
+
+#print @ALLwords
+
+#print @ALLwords[0]
+#print @ALLwords.length
+
+#system 'clear'
+
+
+
+    (0..@ALLwords.length-1).each do |i|
+        (0..@ALLwords.length-1).each do |j|
+        combine = @ALLwords[j]+@ALLwords[i]
+        @ALLcombinations << combine
+      end
+    end
+
+
+#    print @ALLcombinations
+#   print @ALLcombinations.length
+
+
+
+    (0..@ALLcombinations.length-1).each do |i|
+        (0..@ALLwords.length-1).each do |j|
+        if @ALLcombinations[i] == @ALLwords[j]
+          #puts "#{@ALLcombinations[i]} si es igual a #{}{@ALLwords[j]} "
+          puts "#{@ALLcombinations[i]}"
+        end
+      end
+    end
